e49b67da777bb8d63c54d5a53f6e24de6ee5b55a
beaever <thebeaever@gmail.com>
2022-11-23 01:10:31 +0900

    jsx flag error 수정

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,7 @@
 const { loadConfigFromFile, mergeConfig } = require('vite');
 
 module.exports = {
+  ...config,
   async viteFinal(config, { configType }) {
     const { config: userConfig } = await loadConfigFromFile(
       path.resolve(__dirname, '../vite.config.ts')
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonProps } from './Button.types';
 /**
  * @param {Pick<ButtonProps,"size">} size - 버튼 크기
  * @param {Pick<ButtonProps,"color">} color - 버튼 색상
- * @todo 기능 더 만들기
+ * @todo
  */
 
 const Button = ({
