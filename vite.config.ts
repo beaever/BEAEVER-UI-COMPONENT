49f63f9382a96eac78fbfaca55bd1153c5db0929
beaever <thebeaever@gmail.com>
2022-12-13 00:20:58 +0900

    yarn berry setting 진행중

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      '@src': path.resolve('src/'),
+      '@components': path.resolve('src/components'),
+    },
   },
 });
