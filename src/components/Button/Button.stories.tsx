afede29aa0aeb4bfb345438e656f90ecbd397fe1
beaever <thebeaever@gmail.com>
2022-12-13 22:46:46 +0900

    setting 수정

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+
+import Button from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const Base = (args: any) => {
+  return <Button {...args} children={<>text</>} />;
+};
