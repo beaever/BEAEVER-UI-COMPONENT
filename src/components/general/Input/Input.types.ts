7fcfe4c65a7d4e8d2b0725906e26f4b94f39b807
lwj-awesome <lwj1267@naver.com>
2022-12-18 19:18:35 +0900

    [Input]
    - 인풋 컴포넌트 기본 UI 정의
    - 인풋 + 서치 모드에 따라 다른 UI 정의
    - 인풋 기본 props 정의

---
+++ src/components/general/Input/Input.types.ts
@@ -0,0 +1,26 @@
+import React, { SetStateAction } from 'react';
+
+export type InputModeType = 'input' | 'search';
+
+export interface InputProps {
+  /** change Event */
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+  /** css를 위한 ClassName */
+  className?: string;
+
+  /** input의 타입을 지정 기본값 'text' */
+  type?: string;
+
+  /** input의 name을 지정 */
+  name?: string;
+
+  /** search mode를 선택 기본값은 input */
+  mode?: InputModeType;
+
+  /** input의 value */
+  value: string;
+
+  /** input의 setState */
+  setState: React.Dispatch<SetStateAction<string>>;
+}
